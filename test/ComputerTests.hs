06e51a3d22e1762f4ae4839d7593c77181f62de2
Dustin Sallings <dustin@spy.net>
2019-12-02 13:39:14 -0800

    Computer example was day 2.  :p

--- test/ComputerTests.hs
+++ test/ComputerTests.hs
@@ -15,14 +15,14 @@ import           Computer
 d1ex :: Instructions
 d1ex = [1,9,10,3,2,3,11,0,99,30,40,50]
 
-testD1Ex :: Assertion
-testD1Ex = assertEqual "" 3500 (V.head $ fromRight undefined $ execute d1ex)
+testD2Ex :: Assertion
+testD2Ex = assertEqual "" 3500 (V.head $ fromRight undefined $ execute d1ex)
 
 testTimeout :: Assertion
 testTimeout = assertEqual "" (Left "timed out") (executeWithin 2 d1ex)
 
 tests :: [TestTree]
 tests = [
-  testCase "Day 1 Example" testD1Ex,
+  testCase "Day 2 Example" testD2Ex,
   testCase "timeout" testTimeout
   ]
