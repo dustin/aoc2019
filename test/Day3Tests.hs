06874be8e8b3aeb508d3d28cf61abac44ccfa385
Dustin Sallings <dustin@spy.net>
2019-12-03 00:58:02 -0800

    Loop the part1 tests.

--- test/Day3Tests.hs
+++ test/Day3Tests.hs
@@ -17,21 +17,18 @@ parseEx :: Text -> [Wire]
 parseEx = parseLit parseAll
 
 testEx :: Assertion
-testEx = assertEqual "" 6 (part1 . parseEx $ "R8,U5,L5,D3\nU7,R6,D4,L4\n")
-
-testEx2 :: Assertion
-testEx2 = assertEqual "" 159 (part1 . parseEx $ "R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83\n")
-
-testEx3 :: Assertion
-testEx3 = assertEqual "" 135 (part1 . parseEx $ "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7\n")
+testEx = mapM_ (\(t, want) -> assertEqual (show t) want (part1 . parseEx $ t)) [
+  ("R8,U5,L5,D3\nU7,R6,D4,L4\n", 6),
+  ("R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83\n", 159),
+  ("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7\n", 135)
+  ]
 
 testPart1 :: Assertion
 testPart1 = assertEqual "" 280 =<< part1 <$> getInput
 
-testEx3_2 :: Assertion
-testEx3_2 = assertEqual "" 30 (part2 . parseEx $ "R8,U5,L5,D3\nU7,R6,D4,L4\n")
+testEx2 :: Assertion
+testEx2 = assertEqual "" 30 (part2 . parseEx $ "R8,U5,L5,D3\nU7,R6,D4,L4\n")
 
--- too low: 10552
 testPart2 :: Assertion
 testPart2 = assertEqual "" 10554 =<< part2 <$> getInput
 
@@ -39,9 +36,6 @@ tests :: [TestTree]
 tests = [
   testCase "ex1" testEx,
   testCase "ex2" testEx2,
-  testCase "ex3" testEx3,
-
-  testCase "ex3 2" testEx3_2,
 
   testCase "part1" testPart1,
   testCase "part2" testPart2
