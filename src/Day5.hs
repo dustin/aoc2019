5f0e09d22e8ba3327d535df2a35e6a1affb97886
Dustin Sallings <dustin@spy.net>
2019-12-07 22:41:28 -0800

    Use the computer parser for day5

--- src/Day5.hs
+++ src/Day5.hs
@@ -9,4 +9,4 @@ import qualified Data.Vector.Unboxed         as V
 import           Computer
 
 getInput :: IO Instructions
-getInput = V.fromList . fmap read . words . map (\x -> if x == ',' then ' ' else x) <$> readFile "input/day5"
+getInput = readInstructions "input/day5"
