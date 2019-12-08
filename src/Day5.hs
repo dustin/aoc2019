7d9ad2ca862dac21305624f40ba5b914f5f599b2
Dustin Sallings <dustin@spy.net>
2019-12-08 11:35:14 -0800

    Day 5 doesn't need many imports since it does almost nothing.

--- src/Day5.hs
+++ src/Day5.hs
@@ -1,11 +1,5 @@
 module Day5 where
 
-import           Control.Lens
-import           Control.Monad               (guard)
-import           Control.Parallel.Strategies (parBuffer, rdeepseq, using)
-import           Data.Either                 (fromRight)
-import qualified Data.Vector.Unboxed         as V
-
 import           Computer
 
 getInput :: IO Instructions
