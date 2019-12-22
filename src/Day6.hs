de8b3df6dca4b11342913258e5dbc9a7ec1a3679
Dustin Sallings <dustin@spy.net>
2019-12-21 18:36:55 -0800

    Day 6 with bfs

--- src/Day6.hs
+++ src/Day6.hs
@@ -5,6 +5,7 @@ module Day6 where
 
 import           Data.Map.Strict      (Map)
 import qualified Data.Map.Strict      as Map
+import           Data.Maybe           (listToMaybe)
 import           Data.Text            (Text, pack)
 import           Data.Tuple           (swap)
 import           Text.Megaparsec      (endBy, many, sepBy)
@@ -38,11 +39,17 @@ countOrbits omap = sum orbits
   where orbits = lu <$> omap
         lu v = maybe 1 succ (Map.lookup v orbits)
 
--- the subtract 2 in there is to remove the endpoints
-countTransfers :: Text -> Text -> OMap -> Maybe Int
-countTransfers f t omap = subtract 2 . fst <$> dijkstra (dmap Map.!) f (== t)
+-- the subtract 3 in there is to remove the endpoints and the first
+-- part of the transfer.  e.g. listToMaybe returns Just
+-- ["SAN","I","D","E","J","K","YOU"] for the small example.  It's
+-- considered four transfers from K to J to E to D to I according to
+-- the example.
+countTransfers :: Text -> Text -> Map Text Text -> Maybe Int
+countTransfers f t omap = fmap (subtract 3 . length) . listToMaybe . filter ((== t) . head) $ bfsOn head nf [f]
   where
-    dmap = fmap (1,) <$> Map.unionWith (<>) (fmap (:[]) omap) rmap
+    nf []       = []
+    nf xs@(x:_) = (:xs) <$> dmap Map.! x
+    dmap = Map.unionWith (<>) (fmap (:[]) omap) rmap
     rmap = Map.fromListWith (<>) . fmap (fmap (:[]) . swap) $ Map.toList omap
 
 part1 :: IO Int
