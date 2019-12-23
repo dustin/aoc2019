b29df73d3bb434680c01056a17b180583e524965
Dustin Sallings <dustin@spy.net>
2019-12-23 14:50:52 -0800

    Deleting more code.

--- src/Day23.hs
+++ src/Day23.hs
@@ -1,13 +1,8 @@
-{-# LANGUAGE FlexibleContexts  #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards   #-}
-{-# LANGUAGE TupleSections     #-}
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 
 module Day23 where
 
 import           Data.List.Extra (chunksOf)
-import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
 import           Data.Maybe      (fromJust)
 
@@ -20,17 +15,11 @@ import           Search
 getInput :: IO Instructions
 getInput = readInstructions "input/day23"
 
-type Network = Map Int Paused
-
-mkNet :: Instructions -> Network
-mkNet prog = Map.fromList [(a, executePause [a] prog) | a <- [0..49]]
-
-type Queue = [(Int, (Int, Int))]
-
 run :: Instructions -> [(Int,Int)]
-run prog = let inet = mkNet prog
+run prog = let inet = newNet
            in go [(n,(-1,-1)) | n <- Map.keys inet] Nothing inet
   where
+    newNet = Map.fromList [(a, executePause [a] prog) | a <- [0..49]]
     go xs nv net = let (nv'@(Just xy), net') = sendMessages nv xs net
                    in xy : go [(0, fromJust nv')] nv' net'
     sendMessages nv [] net = (nv, net)
