f9e5f8b33c0465e88dfff246819883cda84fb52a
Dustin Sallings <dustin@spy.net>
2019-12-23 23:47:33 -0800

    Part 2 passing

--- src/Day24.hs
+++ src/Day24.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE TupleSections #-}
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 
 module Day24 where
 
 import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
+import qualified Data.Set        as Set
 
 import           AoC
 import           Search
@@ -15,34 +17,81 @@ type World = Map Point Char
 getInput :: FilePath -> IO World
 getInput fp = parseGrid id <$> readFile fp
 
-adjacency :: World -> Point -> Int
-adjacency m = length . filter (== '#') . map (\p' -> Map.findWithDefault '.' p' m) . around
+adjacency :: Ord k => (k -> [k]) -> Map k Char -> k -> Int
+adjacency arnd m = length . filter (== '#') . map (\p' -> Map.findWithDefault '.' p' m) . arnd
 
-bugs :: World -> World
-bugs = Map.filter (== '#')
+bugs :: Map k Char -> [k]
+bugs = Map.keys . Map.filter (== '#')
 
-spaces :: World -> World
-spaces = Map.filter (== '.')
+spaces :: Ord k => (k -> [k]) -> Map k Char -> [k]
+spaces arnd m = Set.toList . Set.fromList . filter (\p -> Map.lookup p m /= Just '#') . concatMap arnd . bugs $ m
 
-automata :: World -> World
-automata m = Map.union frombugs fromspaces
+automata :: Ord k => (k -> [k]) -> Map k Char -> Map k Char
+automata arnd m = Map.unions [frombugs, fromspaces, m]
   where
-    adjCounts = Map.mapWithKey (\k _ -> adjacency m k) m
-    frombugs = Map.fromList [(k, if adjCounts Map.! k == 1 then '#' else '.') | k <- Map.keys (bugs m)]
-    fromspaces = Map.fromList [(k, if (adjCounts Map.! k) `elem` [1,2] then '#' else '.') | k <- Map.keys (spaces m)]
+    adjc k = adjacency arnd m k
+    frombugs = Map.fromList [(k, if adjc k == 1 then '#' else '.') | k <- bugs m]
+    fromspaces = Map.fromList [(k, '#')| k <- spaces arnd m, shouldSpace k]
+    shouldSpace k = Map.lookup k m == Just '.' && adjc k `elem` [1,2]
+
+automata3 :: Ord k => (k -> [k]) -> Map k Char -> Map k Char
+automata3 arnd m = Map.union frombugs fromspaces
+  where
+    adjc k = adjacency arnd m k
+    frombugs = Map.fromList [(k, if adjc k == 1 then '#' else '.') | k <- bugs m]
+    fromspaces = Map.fromList [(k, '#')| k <- spaces arnd m, shouldSpace k]
+    shouldSpace k = Map.lookup k m /= Just '#' && adjc k `elem` [1,2]
 
 displayWorld :: World -> IO ()
-displayWorld m = putStrLn $ drawString m (\p -> Map.findWithDefault ' ' p m)
+displayWorld m = putStrLn $ drawString m (\p -> Map.findWithDefault '.' p m)
 
 part1 :: World -> Int
-part1 m = let (_,_,m') = findCycle id (iterate automata m) in
+part1 m = let (_,_,m') = findCycle id (iterate (automata around) m) in
             bioDiv m'
   where
     ((mnx,mny),(mxx,mxy)) = bounds2d m
 
     bmap = Map.fromList $ zip [(x,y) | y <- [mny..mxy], x <- [mnx..mxx]] [2^x | x <- [0..]]
-    bioDiv :: World -> Int
     bioDiv = sum . map ((bmap Map.!) . fst) . filter ((== '#') . snd) . Map.toList
 
-part2 :: World -> Int
-part2 = const 0
+type Point3 = (Int,Int,Int)
+type World3 = Map Point3 Char
+
+upgrade :: World -> World3
+upgrade = Map.fromList . map (\((x,y),c) -> ((x,y,0),c)) . Map.toList
+
+up :: Point3 -> [Point3]
+up (_,0,z) = [(2,1,z-1)]
+up (2,3,z) = [(x,4,z+1) | x <- [0..4]]
+up (x,y,z) = [(x,y-1,z)]
+
+left :: Point3 -> [Point3]
+left (0,_,z) = [(1,2,z-1)]
+left (3,2,z) = [(4,y,z+1) | y <- [0..4]]
+left (x,y,z) = [(x-1,y,z)]
+
+right :: Point3 -> [Point3]
+right (4,_,z) = [(3,2,z-1)]
+right (1,2,z) = [(0,y,z+1) | y <- [0..4]]
+right (x,y,z) = [(x+1,y,z)]
+
+down :: Point3 -> [Point3]
+down (_,4,z) = [(2,3,z-1)]
+down (2,1,z) = [(x,0,z+1) | x <- [0..4]]
+down (x,y,z) = [(x,y+1,z)]
+
+around3 :: Point3 -> [Point3]
+around3 p = mconcat [up p, left p, right p, down p]
+
+depths :: World3 -> [Int]
+depths = Set.toList . Set.fromList . map thrd . Map.keys
+
+displayWorld3 :: World3 -> IO ()
+displayWorld3 m3 = mapM_ (\l -> putStrLn ("Level " <> show l) >> displayWorld (downgrade l)) $ depths m3
+  where downgrade l = Map.fromList . map (\((x,y,_),c) -> ((x,y),c)) . filter ((== l) . thrd . fst) . Map.toList $ m3
+
+part2 :: Int -> World -> Int
+part2 mins m = let m3 = upgrade m
+                   gens = iterate (automata3 around3) m3
+                   fstate = gens !! mins
+               in length . bugs $ fstate
--- test/Day24Tests.hs
+++ test/Day24Tests.hs
@@ -11,12 +11,13 @@ import           Day24
 testPart1 :: Int -> FilePath -> Assertion
 testPart1 exp fp = assertEqual "" exp =<< part1 <$> getInput fp
 
-testPart2 :: Assertion
-testPart2 = assertEqual "" 0 =<< part2 <$> getInput "input/day24"
+testPart2 :: Int -> Int -> FilePath -> Assertion
+testPart2 exp mins fp = assertEqual "" exp =<< part2 mins <$> getInput fp
 
 tests :: [TestTree]
 tests = [
   testCase "Example 1" (testPart1 2129920 "input/day24.example"),
-  testCase "Part 1" (testPart1 0 "input/day24"),
-  testCase "Part 2" testPart2
+  testCase "Part 1" (testPart1 23967691 "input/day24"),
+  testCase "Example 2" (testPart2 99 10 "input/day24.example"),
+  testCase "Part 2" (testPart2 2003 200 "input/day24")
   ]
